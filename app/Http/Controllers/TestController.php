c36b9a5a7c9bbf118f851bc04c783d02f2ac7e3b
CC <apple@appledeMacBook-Air.local>
2020-11-10 07:44:36 +0800

    1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -70,7 +70,7 @@ class TestController extends Controller
                 file_put_contents('wx_event.log',$xml_data);
                 $data=simplexml_load_string($xml_data);
                 $Content ="欢迎再次关注成功";
-                file_put_contents('wx_event.log',$Content);
+//                file_put_contents('wx_event.log',$Content);
     
                 $result = $this->infocodl($data,$Content);
                 return $result;
